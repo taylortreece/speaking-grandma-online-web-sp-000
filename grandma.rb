8664fc859218061f1aef192462b63cf3815ba5a0
Taylor Treece <tayloratreece@gmail.com>
2020-12-23 19:08:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase.upcase == "I love you grandma!"
-    return "I LOVE YOU TOO PUMPKIN"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 # Whatever you say to grandma, she should respond with
